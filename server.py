dfeb6dbe70f0b0e623ed5c87b0bf04927630e30d
BELARTCOM <belartcom2009@gmail.com>
2022-06-08 19:37:01 +0300

    Function get_fibonacci_num work with digit values

--- server.py
+++ server.py
@@ -1,24 +1,38 @@
-# Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
 hostName = "localhost"
 serverPort = 8080
 
+def str_to_int(string):
+    return int(string.lstrip('/'))
+
+def int_to_str(integer):
+    return str(integer)
+
+def get_fibonacci_num(fibonacci):
+    fib_list = [0, 1]
+    for i in range(1, fibonacci):
+        fib_list_0 = fib_list[0]
+        fib_list_1 = fib_list[1]
+        fib_list[0] = fib_list_1
+        fib_list[1] = fib_list_0 + fib_list_1
+    return fib_list[1]
+
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile.write(bytes('<html><head><title>https://pythonbasics.org</title><link rel="shortcut icon" href="https://www.google.com/favicon.ico" /></head>', 'utf-8'))
+        self.wfile.write(bytes('<p>Fibonacci number = %s</p>'  % int_to_str(get_fibonacci_num(str_to_int(self.path))), 'utf-8'))
+        self.wfile.write(bytes('<body>', 'utf-8'))
+        self.wfile.write(bytes('<p>This web server calculated fibonacci number.</p>', 'utf-8'))
+        self.wfile.write(bytes('</body></html>', 'utf-8'))
 
-if __name__ == "__main__":        
+if __name__ == '__main__':        
     webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+    print('Server started http://%s:%s' % (hostName, serverPort))
 
     try:
         webServer.serve_forever()
@@ -26,4 +40,4 @@ if __name__ == "__main__":
         pass
 
     webServer.server_close()
-    print("Server stopped.")
+    print('Server stopped.')
---
+++ test_server.py
@@ -0,0 +1,12 @@
+import unittest
+from server import get_fibonacci_num
+
+class ServerCorrectReturn(unittest.TestCase):
+
+    def test_result(self):
+        self.assertEqual(get_fibonacci_num(2), 1)
+        self.assertEqual(get_fibonacci_num(3), 2)
+        self.assertEqual(get_fibonacci_num(4), 3)
+        self.assertEqual(get_fibonacci_num(5), 5)
+        self.assertEqual(get_fibonacci_num(6), 8)
+
