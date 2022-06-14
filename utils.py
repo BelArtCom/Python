e618af332ddc00faf9ad2ee1b86738c2fcfd2680
BELARTCOM <belartcom2009@gmail.com>
2022-06-14 19:28:27 +0300

    Fix Fibonacci calculations

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
-def get_fibonacci_num(fibonacci):
+def get_fibonacci_num(index):
+    if index == 0:
+        return 0
+    
+    if index == 1:
+        return 1
+        
     fib_list = [0, 1]
-    for i in range(1, fibonacci):
+    for i in range(1, index):
         fib_list_0 = fib_list[0]
         fib_list_1 = fib_list[1]
         fib_list[0] = fib_list_1
