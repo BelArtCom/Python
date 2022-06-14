3deb66c99791b9ccbaeccb62f97a7da29e0f7e36
BELARTCOM <belartcom2009@gmail.com>
2022-06-14 21:51:46 +0300

    Add factorial test

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
-def get_fibonacci_num(index):
+def get_factorial_num(index):
     if index == 0:
         return 0
     
     if index == 1:
         return 1
 
-    return get_fibonacci_num(index - 1) + get_fibonacci_num(index - 2)
+    return get_factorial_num(index - 1) + get_factorial_num(index - 2)
 
 def get_factorial_num(index):
     if index == 0:
